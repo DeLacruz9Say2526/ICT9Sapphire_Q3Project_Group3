2a8a2561843dcc917cd9778b7e99df3d95399844
DeLara9S-ay2526 <delara.xq@obmontessori.edu.ph>
2026-03-10 23:48:14 +0800

    Add files via upload

---
+++ proj.js
@@ -0,0 +1,409 @@
+function seeFare(){
+    let sta1 = document.getElementById("pick1").value = 50;
+    let sta2 = document.getElementById("pick2").value = 50;
+    let sta3 = document.getElementById("pick3").value = 50;
+    let sta4 = document.getElementById("pick4").value = 50;
+    let sta5 = document.getElementById("pick5").value = 50;
+    let sta6 = document.getElementById("pick6").value = 50;
+    let sta7 = document.getElementById("pick7").value = 50;
+    let sta8 = document.getElementById("pick8").value = 50;
+    let sta9 = document.getElementById("pick9").value = 50;
+    let sta10 = document.getElementById("pick10").value = 50;
+    let sta11 = document.getElementById("pick11").value = 50;
+    let sta12 = document.getElementById("pick12").value = 50;
+    let sta13 = document.getElementById("pick13").value = 50;
+    let tO = document.getElementById("tO");
+    let dA = document.getElementById("dA");
+    let zA = document.getElementById("zA");
+    let pA = document.getElementById("pA");
+    let iZ = document.getElementById("iZ");
+    let rE = document.getElementById("rE");
+    let tE = document.getElementById("tE");
+    let aO = document.getElementById("aO");
+    let aS = document.getElementById("aS");
+    let kaN = document.getElementById("kaN");
+    let saN = document.getElementById("saN");
+    let iG = document.getElementById("iG");
+    let lO = document.getElementById("lO");
+    let preFare1;
+    if (sta1){
+        if (tO){
+            preFare1 = Number(0)
+        }
+        if (dA){
+            preFare1 = Number(0)
+        }
+        if (zA){
+            preFare1 = Number(0.389 * 15)
+        }
+        if (pA){
+            preFare1 = Number(1.746 * 15)
+        }
+        if (iZ){
+            preFare1 = Number(2.98 * 15)
+        }
+        if (rE){
+            preFare1 = Number(3.908 * 15)
+        }
+        if (tE){
+            preFare1 = Number(4.983 * 15)
+        }
+        if (aO){
+            preFare1 = Number(6.147 * 15)
+        }
+        if (aS){
+            preFare1 = Number(7.585 * 15)
+        }
+        if (kaN){
+            preFare1 = Number(8.54 * 15)
+        }
+        if (saN){
+            preFare1 = Number(10.51 * 15)
+        }
+        if (iG){
+            preFare1 = Number(11.633 * 15)
+        }
+        if (lO){
+            preFare1 = Number(13.967 * 15)
+        }
+    }
+    let preFare2;
+    if (sta2){
+        if (tO){
+            preFare2 = Number(0)
+        }
+        if (dA){
+            preFare2 = Number(0)
+        }
+        if (zA){
+            preFare2 = Number(0)
+        }
+        if (pA){
+            preFare2 = Number(0.746 * 15)
+        }
+        if (iZ){
+            preFare2 = Number(1.98 * 15)
+        }
+        if (rE){
+            preFare2 = Number(2.908 * 15)
+        }
+        if (tE){
+            preFare2 = Number(3.983 * 15)
+        }
+        if (aO){
+            preFare2 = Number(5.147 * 15)
+        }
+        if (aS){
+            preFare2 = Number(6.585 * 15)
+        }
+        if (kaN){
+            preFare2 = Number(7.54 * 15)
+        }
+        if (saN){
+            preFare2 = Number(9.51 * 15)
+        }
+        if (iG){
+            preFare2 = Number(10.633 * 15)
+        }
+        if (lO){
+            preFare2 = Number(12.917 * 15)
+        }
+    }
+    let preFare3;
+    if (sta3){
+        if (tO){
+            preFare3 = Number(0.439 * 15)
+        }
+        if (dA){
+            preFare3 = Number(0)
+        }
+        if (zA){
+            preFare3 = Number(0)
+        }
+        if (pA){
+            preFare3 = Number(0)
+        }
+        if (iZ){
+            preFare3 = Number(0.591 * 15)
+        }
+        if (rE){
+            preFare3 = Number(1.519 * 15)
+        }
+        if (tE){
+            preFare3 = Number(2.594 * 15)
+        }
+        if (aO){
+            preFare3 = Number(3.758 * 15)
+        }
+        if (aS){
+            preFare3 = Number(5.196 * 15)
+        }
+        if (kaN){
+            preFare3 = Number(6.151 * 15)
+        }
+        if (saN){
+            preFare3 = Number(8.121 * 15)
+        }
+        if (iG){
+            preFare3 = Number(9.244 * 15)
+        }
+        if (lO){
+            preFare3 = Number(11.528 * 15)
+        }
+    }
+    let preFare4;
+    if (sta4){
+        if (tO){
+            preFare4 = Number(1.796 * 15)
+        }
+        if (dA){
+            preFare4 = Number(0.796 * 15)
+        }
+        if (zA){
+            preFare4 = Number(0)
+        }
+        if (pA){
+            preFare4 = Number(0)
+        }
+        if (iZ){
+            preFare4 = Number(0)
+        }
+        if (rE){
+            preFare4 = Number(0.162 * 15)
+        }
+        if (tE){
+            preFare4 = Number(1.237 * 15)
+        }
+        if (aO){
+            preFare4 = Number(2.041 * 15)
+        }
+        if (aS){
+            preFare4 = Number(3.839 * 15)
+        }
+        if (kaN){
+            preFare4 = Number(4.794 * 15)
+        }
+        if (saN){
+            preFare4 = Number(6.764 * 15)
+        }
+        if (iG){
+            preFare4 = Number(7.887 * 15)
+        }
+        if (lO){
+            preFare4 = Number(10.171 * 15)
+        }
+    }
+    let preFare5;
+    if (sta5){
+        if (tO){
+            preFare5 = Number(3.03 * 2)
+        }
+        if (dA){
+            preFare5 = Number(1.98 * 2)
+        }
+        if (zA){
+            preFare5 = Number(0.591 * 2)
+        }
+        if (pA){
+            preFare5 = Number(0)
+        }
+        if (iZ){
+            preFare5 = Number(0)
+        }
+        if (rE){
+            preFare5 = Number(0)
+        }
+        if (tE){
+            preFare5 = Number(0)
+        }
+        if (aO){
+            preFare5 = Number(1.167 * 15)
+        }
+        if (aS){
+            preFare5 = Number(2.605 * 15)
+        }
+        if (kaN){
+            preFare5 = Number(3.56 * 15)
+        }
+        if (saN){
+            preFare5 = Number(5.53 * 15)
+        }
+        if (iG){
+            preFare5 = Number(6.653 * 15)
+        }
+        if (lO){
+            preFare5 = Number(8.937 * 15)
+        }
+    }
+    let preFare6;
+    if (sta6){
+        if (tO){
+            preFare6= Number(3.958 * 15)
+        }
+        if (dA){
+            preFare6 = Number(2.908 * 15)
+        }
+        if (zA){
+            preFare6 = Number(1.519 * 15)
+        }
+        if (pA){
+            preFare6 = Number(0.162 * 15)
+        }
+        if (iZ){
+            preFare6 = Number(0)
+        }
+        if (rE){
+            preFare6 = Number(0)
+        }
+        if (tE){
+            preFare6 = Number(0)
+        }
+        if (aO){
+            preFare6 = Number(0.513 * 15)
+        }
+        if (aS){
+            preFare6 = Number(1.951 * 15)
+        }
+        if (kaN){
+            preFare6 = Number(2.906 * 15)
+        }
+        if (saN){
+            preFare6 = Number(4.876 * 15)
+        }
+        if (iG){
+            preFare6 = Number(6 * 15)
+        }
+        if (lO){
+            preFare6 = Number(8.283 * 15)
+        }
+    }
+
+    let preFare12;
+    if (sta12){
+        if (tO){
+            preFare12 = Number(11.689 * 15)
+        }
+        if (dA){
+            preFare12 = Number(10.633 * 15)
+        }
+        if (zA){
+            preFare12 = Number(9.244 * 15)
+        }
+        if (pA){
+            preFare12 = Number(7.887 * 15)
+        }
+        if (iZ){
+            preFare12 = Number(6.653 * 15)
+        }
+        if (rE){
+            preFare12 = Number(5.725 * 15)
+        }
+        if (tE){
+            preFare12 = Number(4.65 * 15)
+        }
+        if (aO){
+            preFare12 = Number(3.486 * 15)
+        }
+        if (aS){
+            preFare12 = Number(2.048 * 15)
+        }
+        if (kaN){
+            preFare12 = Number(1.093 * 15)
+        }
+        if (saN){
+            preFare12 = Number(0 * 15)
+        }
+        if (iG){
+            preFare12 = Number(0 * 15)
+        }
+        if (lO){
+            preFare12 = Number(0.287 * 15)
+        }
+    }
+    let preFare13;
+    if (sta13){
+        if (tO){
+            preFare13 = Number(13.967 * 15)
+        }
+        if (dA){
+            preFare13 = Number(12.917 * 15)
+        }
+        if (zA){
+            preFare13 = Number(11.528 * 15)
+        }
+        if (pA){
+            preFare13 = Number(10.171 * 15)
+        }
+        if (iZ){
+            preFare13 = Number(8.937 * 15)
+        }
+        if (rE){
+            preFare13 = Number(8.009 * 15)
+        }
+        if (tE){
+            preFare13 = Number(6.934 * 15)
+        }
+        if (aO){
+            preFare13 = Number(5.77 * 15)
+        }
+        if (aS){
+            preFare13 = Number(4.332 * 15)
+        }
+        if (kaN){
+            preFare13 = Number(3.377 * 15)
+        }
+        if (saN){
+            preFare13 = Number(1.407 * 15)
+        }
+        if (iG){
+            preFare13 = Number(0.284 * 15)
+        }
+        if (lO){
+            preFare13 = Number(0 * 15)
+        }
+    }
+    let fareTotal
+    if (sta1){
+        fareTotal = Number(preFare1 + 50)
+    }
+    if (sta2){
+        fareTotal = Number(preFare2 + 50)
+    }
+    if (sta3){
+        fareTotal = Number(preFare3 + 50)
+    }
+    if (sta4){
+        fareTotal = Number(preFare4 + 50)
+    }
+    if (sta5){
+        fareTotal = Number(preFare5 + 50)
+    }
+    if (sta6){
+        fareTotal = Number(preFare6 + 50)
+    }
+    if (sta7){
+        fareTotal = Number(preFare7 + 50)
+    }
+    if (sta8){
+        fareTotal = Number(preFare8 + 50)
+    }
+    if (sta9){
+        fareTotal = Number(preFare9 + 50)
+    }
+    if (sta10){
+        fareTotal = Number(preFare10 + 50)
+    }
+    if (sta11){
+        fareTotal = Number(preFare11 + 50)
+    }
+    if (sta12){
+        fareTotal = Number(preFare12 + 50)
+    }
+    if (sta13){
+        fareTotal = Number(preFare13 + 50)
+    }
+    document.getElementById("fare").innerHTML = `<h4>PHP ${fareTotal}</h4>`;
+}
+function discFare(){
+    let discFare = Number(fareTotal / 2)
+}
